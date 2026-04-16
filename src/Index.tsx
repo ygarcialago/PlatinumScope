68b8b4673160f2f5123c5f4ce7823d6f14176f72
yoelgarcialago <yoelgarcialago@gmail.com>
2026-04-16 17:52:57 +0200

    Div para almacenar componentes con el mismo ancho

--- src/Index.tsx
+++ src/Index.tsx
@@ -4,14 +4,18 @@ import SearchBar from "./components/SearchBar";
 import Footer from "./components/Footer";
 import './styles/general/index.css'
 import FeaturedReview from "./components/mainPage/FeaturedReview";
+import CategorySection from "./components/mainPage/CategorySection";
 
 function IndexPage() {
   return (
     <>
       <SearchBar />
       <main>
+        <div className="pageContainer">
         {/* <Intro /> */}
         <FeaturedReview />
+        <CategorySection/>
+        </div>
       </main>
       <Footer />
     </>
